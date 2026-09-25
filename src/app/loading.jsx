7e6be59544b871259bce094447362174ed54c25d
Raad904 <raad73575@gmail.com>
2026-09-25 11:58:11 +0600

    add loding

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <span className="loading loading-ring loading-xl scale-150"></span>
+        </div>
+    );
+};
+
+export default Loading;
